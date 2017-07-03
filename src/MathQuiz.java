524adf886296f30669a90d535f7e7a7a177b18e4
Megg Gawat <megawatt@live.com>
2017-07-03 09:23:54 -0400

    Created first addition problem
    Created addition problem

--- src/MathQuiz.java
+++ src/MathQuiz.java
@@ -1,4 +1,16 @@
 
+
 public class MathQuiz {
 
+	public static void main(String[] args) {
+		AdditionProblem test = new AdditionProblem();
+		System.out.println(test.getProblem());
+		int answer = TextIO.getInt();
+		if (answer == test.getAnswer()) {
+			System.out.println("You got it right!");
+		} else {
+			System.out.println("You got it wrong...");
+		}
+	}
+	
 }
